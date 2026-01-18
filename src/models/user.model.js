98d4a71af3ac03cd780ec873cd02c8a0e7882513
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-18 20:23:57 +0530

    added user model

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    trim: true,
+  },
+  isVerified:{
+    type:Boolean,
+    default:false
+  }
+});
