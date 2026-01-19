5e89114936931cf4851068063381a0811cec18fc
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-19 15:01:12 +0530

    added redis

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import app from "./src/app.js";
 import config from "./src/config/environment.js"
 import { connecDb } from "./src/config/database.js";
+import { connectRedis } from "./src/config/redis.js";
 
 
 const {PORT} = config;
 const startServer = async()=>{
     try {
         await connecDb();
+
+        await connectRedis();
 
         app.listen(PORT,()=>{
             console.log(`Server connected on port ${PORT}`);
