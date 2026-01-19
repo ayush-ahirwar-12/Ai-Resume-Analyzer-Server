82dcebd361173a7433039ddb2b82db803ec7e4d6
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-19 12:32:57 +0530

    completed user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,34 +1,58 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    roleId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "role",
+      index: true,
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
+  {
+    timestamps: true,
   },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-  },
-  isVerified:{
-    type:Boolean,
-    default:false
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
   }
+  next();
 });
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+export default mongoose.model("users", userSchema);
