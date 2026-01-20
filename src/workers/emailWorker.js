a08f1a5d2189bc9b55d047bb4a0f9825b4a3905f
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-20 17:51:30 +0530

    ..

---
+++ src/workers/emailWorker.js
@@ -0,0 +1,33 @@
+import { tryCatch, Worker } from "bullmq";
+import connection from "../config/bullmq-connection";
+
+const worker = new Worker('email',async(job)=>{
+    console.log(`processing job ${job.id} - ${job.name}`);
+
+    try {
+        if(job.name==='verification-mail'){
+            
+        }
+    } catch (error) {
+        console.error(`job ${job.id} failed ${error.message}`,{error})
+        throw error;
+    }
+
+},{
+    connection,
+    concurrency:5
+});
+
+worker.on('completed',(job)=>{
+    console.log(`job ${job.id} ${job.name} is completed`);
+})
+
+worker.on('failed', (job, err) => {
+  console.error(`Job ${job?.id} (${job?.name}) failed: ${err.message}`, { error: err });
+});
+
+worker.on('error', (err) => {
+  console.error('Worker encountered an error:', { error: err });
+});
+
+console.info('BullMQ Email Worker started — ready to process email jobs!');
