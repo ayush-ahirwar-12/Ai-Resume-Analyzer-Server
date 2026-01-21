a9076387ebf1fa6f7901cc35ec82691f249c4320
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-21 17:21:33 +0530

    ADDED BREVO

--- src/config/environment.js
+++ src/config/environment.js
@@ -6,5 +6,6 @@ export default {
     PORT:process.env.PORT,
     REDIS_PORT:process.env.REDIS_PORT,
     REDIS_HOST:process.env.REDIS_HOST,
-    REDIS_PASSWORD:process.env.REDIS_PASSWORD
+    REDIS_PASSWORD:process.env.REDIS_PASSWORD,
+    BREVO_API_KEY:process.env.BREVO_API_KEY
 }
