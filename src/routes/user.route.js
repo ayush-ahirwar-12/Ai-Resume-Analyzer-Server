2ec5e99aaf8f1b54d6ce638876d56e0ccd16a5bd
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-01-21 15:51:38 +0530

    register controller

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import {register} from "../controllers/auth.controller.js";
+import authController from "../controllers/auth.controller.js";
+
 const router = express.Router();
 
-router.post("/register",register);
+
+router.post("/register",authController.register);
 
 export default router;
