74e50b974c9090d72f7829148916732fa1753091
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-06 20:55:44 +0530

    changes

--- src/repositories/implementations/MongoCacheRepository.js
+++ src/repositories/implementations/MongoCacheRepository.js
@@ -8,22 +8,22 @@ class MongoCacheRepository extends ICacheRepository {
       return data ? JSON.parse(data) : null;
     } catch (error) {
       throw new Error("Failed to get cache", 500, error);
-    }
-  }
+    };
+  };
   async set(key, value, ttl) {
     try {
       await redisClient.setEx(key, ttl, JSON.stringify(value));
     } catch (error) {
       throw new Error("Failed to set cache", 500,error);
-    }
-  }
+    };
+  };
   async del(key) {
     try {
       await redisClient.del(key);
     } catch (error) {
       throw new Error("Failed to delete cache", 500, error);
-    }
-  }
+    };
+  };
 };
 
 export default MongoCacheRepository;
