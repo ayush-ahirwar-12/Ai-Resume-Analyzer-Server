00d247645efd46bfd96ac193e179cd307daba001
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-08 23:58:28 +0530

    changes

--- src/repositories/contracts/IUserRepository.js
+++ src/repositories/contracts/IUserRepository.js
@@ -3,13 +3,13 @@ class IUserRepository {
     throw new Error("Method not Implemented");
   };
   async findUserbyEmail(email) {
-    throw new Error("method not implemented");
+    throw new Error("method not implemented")
   };
   async findUserbyId(id) {
     throw new Error("method not implemented");
   };
   async update(userId,newData){
-    throw new Error("method not implemented");
+    throw new Error("method not implemented")
   };
 };
 
