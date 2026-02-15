caefe2b106a656123708fb31c7cef5254275cbdd
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-15 23:55:11 +0530

    initial

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,7 +4,7 @@ import config from "../config/environment.js";
 class AuthService{
     verifyToken(token){
         return jwt.sign(token,config.JWT_SECRET)
-    }
+    };
 };
 
 export default AuthService;
