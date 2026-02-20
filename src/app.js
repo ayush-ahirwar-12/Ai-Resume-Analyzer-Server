24057c9d27b625a3c6aca531ff1e871db406e507
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-20 18:23:24 +0530

    initial

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ import express from "express"
 import cookie from "cookie-parser";
 import AuthRoute from "./routes/user.route.js"
 
-const app = express();
-app.use(express.json());
-app.use(cookie());
+const app = express()
+app.use(express.json())
+app.use(cookie())
 
 app.use("/api/auth",AuthRoute)
 
 
-export default app;
+export default app
