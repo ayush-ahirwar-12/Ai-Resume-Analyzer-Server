d2c8813552d5e61a47c38d1f0b8c8201df86a650
Ayush Ahirwar <ayushahirwar04@gmail.com>
2026-02-26 23:51:12 +0530

    completed create role controller

--- src/services/role.service.js
+++ src/services/role.service.js
@@ -3,17 +3,17 @@ import { AppError } from "../utils/errors";
 
 class RoleService {
   constructor() {
-    this.roleRepository = new mongoRoleRepository();
+    this.roleRepository = new mongoRoleRepository()
   }
   async createRole(RoleData) {
     const existingRole = await this.roleRepository.findRoleByName(
       RoleData.name,
-    );
+    )
     if (role) {
-      throw new AppError("Role already exists", 400);
+      throw new AppError("Role already exists", 400)
     }
-    return await this.roleRepository.createRole(RoleData);
+    return await this.roleRepository.createRole(RoleData)
   }
 }
 
-export default RoleService;
+export default RoleService
