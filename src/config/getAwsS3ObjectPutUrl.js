a5cbb428e1d797cc70074eee4dbc9dff78c74f10
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-02 19:29:09 +0530

    created resume upload url controller

--- src/config/getAwsS3ObjectPutUrl.js
+++ src/config/getAwsS3ObjectPutUrl.js
@@ -1,6 +1,7 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { GetObjectCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import config from "./environment.js"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { AppError } from "../utils/errors.js";
 
 const { AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_BUCKET_NAME } = config;
 
@@ -12,22 +13,40 @@ export const s3 = new S3Client({
     }
 })
 
-export const putObjectS3 = async (req, res) => {
+export const putObjectS3 = async (fileName, fileType) => {
     try {
-        const {contentType } = req.body;
-        if (!contentType) {
-            throw new Error("Please provide the fileName and contentType ");
-        }
+        const key = `resumes/${Date.now()}-${fileName}`;
 
         const command = new PutObjectCommand({
             Bucket: AWS_BUCKET_NAME,
-            Key: `resumes/${Date.now()}-jj`,
+            Key: key,
             ContentType: contentType
         })
         const url = await getSignedUrl(s3, command, { expiresIn: 60 });
-        res.status(200).json(url)
+        return { url, key }
     } catch (error) {
-        res.status(500).json(error?.message);
-        console.log(error);
+        throw new AppError("Failed to generate aws url", 400);
     }
+}
+
+export const extractedTextFromS3 = async (key) => {
+    const command = new GetObjectCommand({
+        Bucket: AWS_BUCKET_NAME,
+        Key: key
+    });
+    const response = await s3.send(command);
+    const streamToBuffer = (stream) => {
+        new promise((resolve, reject) => {
+            const chunks = [];
+            stream.on("data", (chunk) => chunks.push(chunk));
+            stream.on("error", reject);
+            stream.on("end", () => resolve(Buffer.concat(chunks)));
+
+        })
+
+    }
+        const buffer= await streamToBuffer(response.Body);
+        const data = await pdfParse(buffer);
+        return data.text;
+
 }
