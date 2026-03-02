e6b47b07b12eb045c44cdd1682fd70505adf3e1f
ayush ahirwar <ayushahirwar04@gmail.com>
2026-03-02 15:32:17 +0530

    initialised aws s3

--- src/config/getAwsS3ObjectPutUrl.js
+++ src/config/getAwsS3ObjectPutUrl.js
@@ -1,7 +1,8 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import config from "./environment.js"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
-const {AWS_ACCESS_KEY,AWS_SECRET_KEY} =config;
+const {AWS_ACCESS_KEY,AWS_SECRET_KEY,AWS_BUCKET_NAME} =config;
 
 export const s3= new S3Client({
     region:"ap-south-1",
@@ -10,3 +11,12 @@ export const s3= new S3Client({
         secretAccessKey:AWS_SECRET_KEY
     }
 })
+
+export const putObject= async (fileName,fileType)=>{
+    const command = new PutObjectCommand({
+        Bucket:AWS_BUCKET_NAME,
+        Key:`resumes/${Date.now()}-${fileName}`,
+        ContentType:fileType
+    })
+    return await getSignedUrl(s3,command,{expiresIn:60});
+}
